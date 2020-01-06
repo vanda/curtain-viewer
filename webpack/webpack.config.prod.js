b9d86124bb48ac236f062525d37ecef460c0c54e
methodog <sittingbottom@gmail.com>
2020-01-06 11:43:32 +0000

    add autoprfixer

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -33,6 +33,7 @@ module.exports = merge(common, {
         use : [
           MiniCssExtractPlugin.loader,
           'css-loader',
+          'postcss-loader',
           'sass-loader'
         ]
       }
