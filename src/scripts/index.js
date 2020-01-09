fe2adaf91352396d514d4277216eca01f655ae73
methodog <sittingbottom@gmail.com>
2020-01-09 17:05:10 +0000

    making colours themeable; mv svg icons to inline JS

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -24,6 +24,7 @@ const iiifLayerStack = {
     el.osd = OpenSeadragon({
       id: `layerstack${iiifLayerStack.id}`,
       showHomeControl: false,
+      showFullPageControl: false,
       zoomInButton: 'layerstack__zoom-in',
       zoomOutButton: 'layerstack__zoom-out'
     });
@@ -31,6 +32,9 @@ const iiifLayerStack = {
     const fullScreen = el.appendChild(document.createElement('div'));
     fullScreen.className = 'layerstack__fullscreen';
     fullScreen.title = 'Full screen';
+    fullScreen.innerHTML = `
+      <svg role="img" viewBox="0 0 100 100"><path d="M12.346 22.572l17.619 17.612 10.219-10.22-17.612-17.618L31.925 3H3v28.925l9.346-9.353zm10.226 65.082l17.612-17.619-10.22-10.219-17.618 17.612L3 68.075V97h28.925l-9.353-9.346zm54.856-75.308L59.816 29.965l10.22 10.219 17.618-17.612L97 31.925V3H68.075l9.353 9.346zm10.226 65.082L70.035 59.816l-10.219 10.22 17.612 17.618L68.075 97H97V68.075l-9.346 9.353z"/></svg>
+    `;
     document.addEventListener('click', (e) => {
       if (e.target.closest('.layerstack__fullscreen')) {
         if (document.fullscreenElement === el) {
@@ -76,7 +80,6 @@ const iiifLayerStack = {
 
     el.key = dash.appendChild(document.createElement('div'));
     el.key.className = 'layerstack__key';
-
     el.labels = dash.appendChild(document.createElement('div'));
     el.labels.className = 'layerstack__labels';
 
@@ -105,15 +108,19 @@ const iiifLayerStack = {
     }
   },
   key: (el, label) => {
+    const icon = '<svg role="img" viewBox="0 0 24 14"><path d="M22.214 7.162L11.622 1.286 1.031 7.162l10.591 5.876 10.592-5.876z"/></svg>';
     el.key.innerHTML = '';
     for (let i = 0; i < el.stackHeight; ++i) {
       el.key.innerHTML += `
-        <div class="layerstack__keyline" style="width:${i*100/(el.stackHeight-1)}%">${i+1}</div>
+        <div class="layerstack__keyline" style="width:${i*100/(el.stackHeight-1)}%"></div>
       `;
     }
+    let keyIcon = [...Array(el.stackHeight)].map(() => { return icon; });
     el.labels.innerHTML += `
       <div class="layerstack__label" data-layerstack-layer="${el.stackHeight}">
-        <div class="layerstack__labelkey">${el.stackHeight}</div>
+        <div class="layerstack__labelkey">
+          ${keyIcon}
+        </div>
         <div class="layerstack__labeltext">${label}</div>
       </div>
     `;
