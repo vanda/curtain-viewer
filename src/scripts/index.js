58e7941fe7d4b0cdea43d598e941509f1dbaf065
methodog <sittingbottom@gmail.com>
2020-02-12 14:39:30 +0000

    add LayerStack object to exports

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-import layerStacker from './layerstack';
+import {default as layerStacker, LayerStack} from './layerstack';
 
 Array.from(document.querySelectorAll('.layerstack'), (layerstackEl) => {
   layerStacker(layerstackEl);
