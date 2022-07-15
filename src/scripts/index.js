e4b5f1ade067530ab5c71ad60d4f9d74a9f44d33
methodog <sittingbottom@gmail.com>
2022-07-15 15:14:30 +0100

    demo: allow IIIF manifest as qrystr at demo url with '?manifest='

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,12 @@
 import curtainViewerLoader, { CurtainViewer } from './curtain-viewer';
 
+/* for purposes of this demo, allow manifest to be set by an optional querystring */
+const qstr = new Map(location.search.slice(1).split('&').map(kv => kv.split('=')));
+if (qstr.has('manifest')) {
+  document.querySelector('.curtain-viewer').dataset.iiifManifest = qstr.get('manifest');
+}
+
+/* activate any curtainViewer elements in the page */
 Array.from(document.querySelectorAll('.curtain-viewer'), (curtainViewerEl) => {
   curtainViewerLoader(curtainViewerEl);
 });
